cc2415674f6a240d3ed2feec9d57869d4d2333f6
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2024-02-19 00:38:15 -0500

    Time: 0 ms (100.00%) | Memory: 11.5 MB (49.01%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -14,8 +14,6 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if (!p and !q) return true;
         if ((!p and q) or (p and !q)) return false; 
-        if (p and q and !p->left and !p->right and !q->left and !q->right) return p->val == q->val;
-        
         return isSameTree(p->left, q->left) and isSameTree(p->right, q->right) and p->val == q->val;
         
     }
