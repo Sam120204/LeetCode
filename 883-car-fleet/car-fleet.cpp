10ae6a24c711abd949735d4508cb52fe110823ec
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2024-03-02 16:47:54 -0500

    Time: 318 ms (10.83%) | Memory: 102.6 MB (5.02%) - LeetSync

---
+++ 883-car-fleet/car-fleet.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        stack<int> fleet;
+        if (position.size() == 1) return 1;
+        map<int, int> ori_pos = {};
+        for (int i = 0; i < position.size(); i++) {
+            ori_pos[position[i]] = i;
+        }
+        //for (const auto& [key, val] : ori_pos) { cout << key << " " << val << endl;}
+        sort(position.rbegin(), position.rend());
+     //   double first = (target - position[0]) / speed[ori_pos[position[0]]];
+        double first = static_cast<double>(target - position[0]) / speed[ori_pos[position[0]]];
+
+        double maxx = first;
+        //cout <<position[0] << " "<<speed[ori_pos[position[0]]] << endl;
+        fleet.push(first);
+        cout <<first<<endl;
+        for (int i = 1; i < position.size(); i++) {
+            double t = time(target, position[i], speed[ori_pos[position[i]]]);
+            if (t > maxx) {
+                fleet.push(t);
+                maxx = max(t, maxx);
+               // cout <<t<<endl;
+            }
+        }
+
+        return fleet.size();
+    }
+    double time(int target, int pos2, double speed2) {
+        return (target - pos2) / speed2;
+    }
+};
