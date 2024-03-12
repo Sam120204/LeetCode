1e5f5bb4f209078723fe9d27f9f92df27f892652
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2024-03-11 23:33:59 -0400

    Time: 6 ms (55.22%) | Memory: 12.8 MB (77.48%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> nodes;
-        Node* h = head;
-        
-        while (h){
-            nodes[h] = new Node(h->val);
-            h = h->next;
+        Node *curr=head;
+
+        while(curr!=NULL)
+        {
+            Node *temp=new Node(curr->val);
+            temp->next=curr->next;
+            curr->next=temp;
+            curr=curr->next->next;
         }
-        h = head;
-        while (h){
-            Node* newNode = nodes[h];
-            newNode->next = nodes[h->next];
-            newNode->random = nodes[h->random];
-            h = h->next;
+        curr=head;
+
+        while(curr!=NULL)
+        {
+            if (curr->random!=NULL)
+            {
+                curr->next->random=curr->random->next;
+            }
+            curr=curr->next->next;
         }
-        return nodes[head];
+
+        curr=head;
+        Node *dummy=new Node(-1);
+        Node *ans=dummy;
+        while(curr!=NULL)
+        {
+            ans->next=curr->next;
+            curr->next=curr->next->next;
+
+            ans=ans->next;
+            curr=curr->next;
+        }
+        return dummy->next;
+
     }
 };
