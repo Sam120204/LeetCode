b3d2db13a1a595cc4bef145e61fab5e1e92013c0
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2024-03-16 23:30:08 -0400

    Time: 5 ms (31.92%) | Memory: 9.2 MB (50.51%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -6,14 +6,13 @@ public:
         if (len == 2) return max(nums[0], nums[1]);
         vector<int> dp(nums.size()+1, 0);
         dp[0] = nums[0], dp[1] = nums[1];
+        int temp_max = nums[0];
         for (int i = 2; i < dp.size(); i++) {
             if (i == dp.size()-1) dp[i] = max(dp[i-1],dp[i-2]);
             else {
-                for (int j=0; j < i-1; j++) {
-                    if (j < i) dp[i] = max(dp[j]+ nums[i], dp[i]);
-                }
-                
+                dp[i] = max(dp[i-1], temp_max+nums[i]);
             }
+            temp_max = max(temp_max, dp[i-1]);
         }
         return dp[len];
     }
