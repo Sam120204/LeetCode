336be3bff4e59bd1fdf398ebbf7c561ba414e3ff
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2024-05-06 15:56:27 -0400

    Time: 19 ms (44.56%) | Memory: 22.1 MB (80.32%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,46 +1,38 @@
+/*
+    Determine if a 9x9 Sudoku board is valid (no repeats)
+
+    Boolean matrices to store seen values. Check rows, cols, 3x3 sub-boxes
+
+    Time: O(cnt^2)
+    Space: O(cnt^2)
+*/
+
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        for (int row = 0; row < 9; row++) {
-            map<char, int> row_dict;
-            map<char, int> col_dict;
-            for (int col = 0; col < 9; col++) {
-                if (board[row][col] != '.' and row_dict.find(board[row][col]) != row_dict.end()) {
-                    return false;
-                }
-                row_dict[board[row][col]]++;
-                if (board[col][row] != '.' and col_dict.find(board[col][row]) != col_dict.end()) {
-                    return false;
-                }
-                col_dict[board[col][row]]++;
-            }
-        }
+        const int cnt = 9;
+        bool row[cnt][cnt] = {false};
+        bool col[cnt][cnt] = {false};
+        bool sub[cnt][cnt] = {false};
         
-        for (int i =0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                int row = i * 3, col = j * 3;
-               
-                map<char, int> grid_dict;
-                for (int m = row; m < row+3; m++) {
-                    for (int n = col; n < col+3; n++) {
-                        if (board[m][n] != '.' and grid_dict.find(board[m][n]) != grid_dict.end()) {
-                            return false;
-                        }
-                        grid_dict[board[m][n]]++;
-                    }
+        for(int r = 0; r < cnt; ++r){
+            for(int c = 0; c < cnt; ++c){
+                if(board[r][c] == '.')
+                    continue; // if not number pass
+                
+                int idx = board[r][c] - '0' - 1; //char to num idx
+                int area = (r/3) * 3 + (c/3);
+                
+                //if number already exists
+                if(row[r][idx] || col[c][idx] || sub[area][idx]){
+                    return false;
                 }
+                
+                row[r][idx] = true;
+                col[c][idx] = true;
+                sub[area][idx] = true;
             }
         }
         return true;
     }
 };
-
-// [".",".",".",".","5",".",".","1","."],
-// [".","4",".","3",".",".",".",".","."],
-// [".",".",".",".",".","3",".",".","1"],
-// ["8",".",".",".",".",".",".","2","."],
-// [".",".","2",".","7",".",".",".","."],
-// [".","1","5",".",".",".",".",".","."],
-// [".",".",".",".",".","2",".",".","."],
-// [".","2",".","9",".",".",".",".","."],
-// [".",".","4",".",".",".",".",".","."]
