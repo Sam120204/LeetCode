1832292ea5db28a34125d2146e609a1c9300d073
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2024-09-07 19:50:07 -0400

    Time: 4 ms (75.31%) | Memory: 13.4 MB (9.06%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -8,17 +8,34 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// pointer: nullptr
+// - pointing to a memory address
+// - how to get access/read to this memory?
+//     int head->val
+//     ListNode* head->next
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-        while (curr != NULL) {
-            ListNode* nextTemp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextTemp;
+        if (!head) return head;
+        stack<ListNode*> lst;
+        lst.push(head);
+        head = head->next;
+        while (head) {
+            lst.push(head);
+            head = head->next;
         }
-        return prev;
+        // stack [5,4,3,2,1]
+        ListNode* res = lst.top(); // 5
+        lst.pop();
+        ListNode* iter = res;// iterate stack
+
+        while (!lst.empty()) {
+            iter->next = lst.top();
+            lst.pop();
+            iter = iter->next;
+        }
+        iter->next = nullptr;
+        return res;
     }
 };
