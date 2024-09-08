201a3b10d2270979e7e875a59a14f309befa6b35
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2024-09-07 20:27:13 -0400

    Time: 0 ms (100.00%) | Memory: 8.5 MB (8.84%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,10 +2,16 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> par;
-        map<char, char> m = {{')', '('}, {'}','{'}, {']','['}};
+        map<char, char> m = {{')', '('}, {']', '['}, {'}', '{'}};
+        // & in C++:
+        // Reference: pass by reference using &
+        // save time, originial element
+        // const auto &: cannot modify and save time 
         for (const auto& ch : s) {
+            // case of left par
             if (m.find(ch) == m.end()) par.push(ch);
-            else { 
+            else {
+                // early return
                 if (par.empty()) return false;
                 if (m[ch] == par.top()) par.pop();
                 else return false;
@@ -14,4 +20,3 @@ public:
         return par.empty();
     }
 };
-
