a9031788502f3d619f9435364aefe03d460de306
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-01-13 15:12:03 -0500

    Time: 17 ms (6.18%) | Memory: 9 MB (51.22%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        // Step 1: Create an array to store the last occurrence of each character
-        int M[256] = {0}; // Fixed size array for ASCII characters
-        for (int i = 0; i < s.size(); ++i) {
-            M[s[i]] = i; // Store the last index of character s[i]
-        }
-
-        // Step 2: Iterate through the string and calculate partitions
-        vector<int> partitions;
-        int start = 0, end = 0;
-
-        for (int i = 0; i < s.size(); ++i) {
-            end = max(end, M[s[i]]); // Update the current partition's end index
-            if (i == end) { // If the current index reaches the partition's end
-                partitions.push_back(end - start + 1); // Add partition size to the result
-                start = i + 1; // Start a new partition
+        map<char, vector<int>> pos;
+        for (int i = 0; i < s.size(); i++) {
+            if (pos.find(s[i]) == pos.end()) {
+                pos[s[i]] = {i, i};
+            } else {
+                pos[s[i]][1] = i;
             }
         }
 
+        for (const auto& pair : pos) {
+            cout << pair.first << ": " << pair.second[0] << " " << pair.second[1] << endl;
+        }
+
+        vector<int> partitions;
+        int start = 0, end = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            end = max(end, pos[s[i]][1]);
+            if (i == end) {
+                partitions.push_back(end-start+1);
+                start=i+1;
+            }
+        }
         return partitions;
     }
 };
