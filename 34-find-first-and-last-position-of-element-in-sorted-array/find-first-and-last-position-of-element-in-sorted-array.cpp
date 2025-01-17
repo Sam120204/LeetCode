a12221dd63debaa62f5383896e9a1ab590f3db76
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-01-17 17:27:02 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (16.95%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,35 +1,39 @@
 class Solution {
+    int findmin(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1, res= -1;
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+            if (nums[mid] == target) {
+                res=mid;
+                r=mid-1;
+            } else if (nums[mid] < target) {
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+        return res;
+    }
+
+    int findmax(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1, res= -1;
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+            if (nums[mid] == target) {
+                res=mid;
+                l=mid+1;
+            } else if (nums[mid] < target) {
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+        return res;
+    }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> res(2, -1);
-        
-        // Find the leftmost (first) occurrence of the target
-        int left = 0, right = nums.size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] >= target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        if (left >= nums.size() || nums[left] != target) {
-            return res; // target not found
-        }
-        res[0] = left;
-        
-        // Find the rightmost (last) occurrence of the target
-        right = nums.size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        res[1] = right;
-        
-        return res;
+        int l = findmin(nums, target);
+        int r = findmax(nums, target);
+        return {l, r};
     }
 };
