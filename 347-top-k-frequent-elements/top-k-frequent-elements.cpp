5de083f95095b744767e61fed3a272a35aa70f62
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-01-25 20:31:11 -0500

    Time: 3 ms (54.21%) | Memory: 19.8 MB (15.99%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-       unordered_map<int, int> m;
-       int max_count = 1;
-       for (auto &i : nums) max_count = max(max_count, ++m[i]);
-       vector<vector<int>> bucket(max_count + 1); // Corrected here
-       
-       for (auto &i : m) {
-           bucket[i.second].push_back(i.first);
-       }
-       
-       vector<int> res;
-       for (int i = max_count; i > -1 && res.size() < k; i--) { // Corrected here
-           for (const int &num : bucket[i]) {
-               res.push_back(num);
-               if (res.size() == k) break;
-           }
-       }
-       
-       return res;
+        unordered_map<int, int> count; // Count the frequency of each element
+        for (auto &i : nums) {
+            count[i]++;
+        }
+
+        // Create buckets where index represents frequency
+        vector<vector<int>> bucket(nums.size() + 1);
+        for (const auto &ele : count) {
+            bucket[ele.second].push_back(ele.first); // Group numbers by frequency
+        }
+
+        vector<int> res;
+        // Iterate through the buckets from the end (highest frequency)
+        for (int i = bucket.size() - 1; i >= 0 && res.size() < k; --i) {
+            for (const auto &num : bucket[i]) {
+                res.push_back(num); // Collect top k frequent elements
+                if (res.size() == k) break;
+            }
+        }
+
+        return res;
     }
 };
