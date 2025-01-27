988b7af17ab23299ccabe3b128a30bb62d76588f
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-01-27 14:21:57 -0500

    Time: 87 ms (20.14%) | Memory: 40.7 MB (10.84%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.cpp
+++ 971-shortest-bridge/shortest-bridge.cpp
@@ -70,12 +70,12 @@ public:
         }
 
 
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                cout << grid[i][j] << " ";
-            }
-            cout << endl;
-        }
+        // for (int i = 0; i < grid.size(); i++) {
+        //     for (int j = 0; j < grid[0].size(); j++) {
+        //         cout << grid[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
         
         return count;
     }
