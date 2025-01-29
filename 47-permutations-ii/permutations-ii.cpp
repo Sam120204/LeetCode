a704dabc3e44587a62f9458406f2bc36bf596dd9
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-01-29 15:13:33 -0500

    Time: 0 ms (100.00%) | Memory: 13.3 MB (37.35%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> path;
-        vector<bool> used(nums.size(), false);
-        sort(nums.begin(), nums.end());  // Step 1: Sort to handle duplicates
-        backtrack(nums, res, path, used);
-        return res;
-    }
-    
-private:
-    void backtrack(vector<int>& nums, vector<vector<int>>& res, vector<int>& path, vector<bool>& used) {
-        if (path.size() == nums.size()) {
-            res.push_back(path);
+    void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
+        if (i == j-1) {
+            res.push_back(num);
             return;
         }
-        
-        for (int i = 0; i < nums.size(); i++) {
-            // Skip used numbers
-            if (used[i]) continue;
-            
-            // Pruning condition to avoid duplicates
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
-
-            // Choose
-            path.push_back(nums[i]);
-            used[i] = true;
-
-            // Explore
-            backtrack(nums, res, path, used);
-            
-            // Undo the choice (backtrack)
-            used[i] = false;
-            path.pop_back();
+        for (int k = i; k < j; k++) {
+            if (i != k && num[i] == num[k]) continue;
+            swap(num[i], num[k]);
+            recursion(num, i+1, j, res);
         }
+    }
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> >res;
+        recursion(num, 0, num.size(), res);
+        return res;
     }
 };
