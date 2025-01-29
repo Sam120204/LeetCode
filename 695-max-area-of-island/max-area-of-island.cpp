23861847e5478431c8b7877177a4ea685d1d0ce4
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-01-29 16:55:49 -0500

    Time: 52 ms (5.56%) | Memory: 44.7 MB (5.07%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,77 +1,33 @@
-// vector<int> direction{-1, 0, 1, 0, -1};
-// int maxAreaOfIsland(vector<vector<int>>& grid) {
-//     int m = grid.size(), n = m ? grid[0].size() : 0, local_area, area = 0, x, y;
-//     for (int i = 0; i < m; ++i) {
-//         for (int j = 0; j < n; ++j) {
-//             if (grid[i][j]) {
-//                 local_area = 1;
-//                 grid[i][j] = 0;
-//                 stack<pair<int, int>> island;
-//                 island.push({i, j});
-//                 while (!island.empty()) {
-//                     auto [r, c] = island.top();
-//                     island.pop();
-//                     for (int k = 0; k < 4; ++k) {
-//                         x = r + direction[k], y = c + direction[k+1];
-//                         if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1) {
-//                             grid[x][y] = 0;
-//                             ++local_area;
-//                             island.push({x, y});
-//                         }
-//                     }
-//                 }
-//                 area = max(area, local_area);
-//             }
-//         }
-//     }
-//     return area;
-// }
-
-// // Method 2:
-// vector<int> direction{-1, 0, 1, 0, -1};
-// int maxAreaOfIsland(vector<vector<int>>& grid) {
-//     if (grid.empty() || grid[0].empty()) return 0;
-//     int max_area = 0;
-//     for (int i = 0; i < grid.size(); ++i) {
-//         for (int j = 0; j < grid[0].size(); ++j) {
-//             if (grid[i][j] == 1) {
-//                 max_area = max(max_area, dfs(grid, i, j));
-//             }
-//         }
-//     }
-//     return max_area;
-// }
-// int dfs(vector<vector<int>>& grid, int r, int c) {
-//     if (grid[r][c] == 0) return 0;
-//     grid[r][c] = 0;
-//     int x, y, area = 1;
-//     for (int i = 0; i < 4; ++i) {
-//         x = r + direction[i], y = c + direction[i + 1];
-//         if (x >= 0 && x < grid.size() && y >= 0 && y < grid[0].size()) {
-//             area += dfs(grid, x, y);
-//         }
-//     }
-//     return area;
-// }
-
-
-
 class Solution {
-public:
-    int dfs(vector<vector<int>>& grid, int r, int c) {
-        // base case:
-        if (r < 0 or r >= grid.size() or c < 0 or c >= grid[0].size() or grid[r][c] == 0) return 0;
-        // here means it is in the bound and the current pos is 1
-        grid[r][c] = 0;
-        return 1 + dfs(grid, r-1,c) + dfs(grid, r+1,c)+ dfs(grid, r,c+1)+ dfs(grid, r,c-1);
+    int dfs(vector<vector<int>>& grid, int row, int col, int& res) {
+        if (grid[row][col] == 0) return 0;
+        grid[row][col] = 2;
+        vector<vector<int>> dir{{-1,0}, {0,1}, {0,-1}, {1,0}};
+        int temp = 1;
+        for (auto & d:dir) {
+            int x_inc = d[0], y_inc = d[1];
+            int new_r = row+x_inc, new_c = col+y_inc;
+            if (new_r >= 0 and new_r <= grid.size()-1 and new_c >= 0 and new_c <= grid[0].size()-1 and grid[new_r][new_c] == 1) {
+                temp += dfs(grid, new_r, new_c, res);
+            }
+
+        }
+        res = max(res, temp);
+        return temp;
     }
+public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int maxx = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                maxx = max(maxx, dfs(grid, i, j));
+        int res = 0;
+        int m = grid.size(), n = grid[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    dfs(grid, i, j, res);
+                }
             }
         }
-        return maxx;
+
+        return res;
     }
 };
