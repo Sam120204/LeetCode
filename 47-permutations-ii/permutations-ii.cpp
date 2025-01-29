f4d2ae379db8e0b9c724eae45e3f5394c4b1ca0a
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-01-29 14:30:58 -0500

    Time: 0 ms (100.00%) | Memory: 11.5 MB (80.24%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,42 +1,38 @@
 class Solution {
-    string encode(vector<int>& vec) {
-        string s;
-        for (int num : vec) {
-            s += to_string(num) + ",";
-        }
-        return s;
-    }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());  // Sorting helps handle duplicates
         vector<vector<int>> res;
-        queue<vector<int>> q;
-        unordered_set<string> seen;
-        
-        q.push({});
-        
-        while (!q.empty()) {
-            vector<int> curr = q.front();
-            q.pop();
-            
-            if (curr.size() == nums.size()) {
-                res.push_back(curr);
-                continue;
-            }
-            
-            for (int i = 0; i < nums.size(); i++) {
-                if (count(curr.begin(), curr.end(), nums[i]) < count(nums.begin(), nums.end(), nums[i])) {
-                    vector<int> next = curr;
-                    next.push_back(nums[i]);
-                    
-                    string key = encode(next);  // Encode permutation
-                    if (!seen.count(key)) {
-                        seen.insert(key);
-                        q.push(next);
-                    }
-                }
-            }
-        }
+        vector<int> path;
+        vector<bool> used(nums.size(), false);
+        sort(nums.begin(), nums.end());  // Step 1: Sort to handle duplicates
+        backtrack(nums, res, path, used);
         return res;
+    }
+    
+private:
+    void backtrack(vector<int>& nums, vector<vector<int>>& res, vector<int>& path, vector<bool>& used) {
+        if (path.size() == nums.size()) {
+            res.push_back(path);
+            return;
+        }
+        
+        for (int i = 0; i < nums.size(); i++) {
+            // Skip used numbers
+            if (used[i]) continue;
+            
+            // Pruning condition to avoid duplicates
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+
+            // Choose
+            path.push_back(nums[i]);
+            used[i] = true;
+
+            // Explore
+            backtrack(nums, res, path, used);
+            
+            // Undo the choice (backtrack)
+            used[i] = false;
+            path.pop_back();
+        }
     }
 };
