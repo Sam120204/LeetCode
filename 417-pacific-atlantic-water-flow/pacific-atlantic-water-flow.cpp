c80dc172230aaaa43c78af10a36639c47ce9ccdb
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-01-31 01:07:44 -0500

    Time: 96 ms (8.34%) | Memory: 48.9 MB (5.04%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,45 +1,35 @@
-#include <vector>
-#include <set>
-
-using namespace std;
-
 class Solution {
-    void dfs(vector<vector<int>>& heights, set<pair<int, int>>& ocean, int r, int c) {
-        static const vector<int> dir = {-1, 0, 1, 0, -1};
-        ocean.insert({r, c});
-        for (int i = 0; i < 4; i++) {
-            int new_r = r + dir[i], new_c = c + dir[i+1];
-            if (new_r >= 0 && new_r < heights.size() && 
-                new_c >= 0 && new_c < heights[0].size() && 
-                heights[new_r][new_c] >= heights[r][c] && 
-                ocean.find({new_r, new_c}) == ocean.end()) {
-                dfs(heights, ocean, new_r, new_c);
+    void dfs(vector<vector<int>>& heights, int r, int c, vector<vector<bool>>& visited) {
+        vector<vector<int>> dir{{-1,0}, {0,1}, {1,0}, {0, -1}};
+        visited[r][c] = true;
+        for (auto & d : dir) {
+            int new_r = r + d[0], new_c = c + d[1];
+            if (new_r >= 0 and new_r < heights.size() and new_c >= 0 and new_c < heights[0].size() and !visited[new_r][new_c] and heights[new_r][new_c] >= heights[r][c]) {
+                dfs(heights, new_r, new_c, visited);
             }
         }
     }
-
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        set<pair<int, int>> pacific, atlantic;
-        int rows = heights.size();
-        int cols = heights[0].size();
+        int m = heights.size(), n = heights[0].size();
+        vector<vector<bool>> pacific(m, vector<bool>(n, false));
+        vector<vector<bool>> atlantic(m, vector<bool>(n, false));
+        vector<vector<int>> result;
 
-        for (int i = 0; i < cols; i++) {
-            dfs(heights, pacific, 0, i);
-            dfs(heights, atlantic, rows - 1, i);
-        }
-        for (int i = 0; i < rows; i++) {
-            dfs(heights, pacific, i, 0);
-            dfs(heights, atlantic, i, cols - 1);
-        }
+        for (int i = 0; i < m; i++) dfs(heights, i, 0, pacific);
+        for (int i = 0; i < n; i++) dfs(heights, 0, i, pacific);
 
-        vector<vector<int>> res;
-        for (const auto& p : pacific) {
-            if (atlantic.find(p) != atlantic.end()) {
-                res.push_back({p.first, p.second});
+        for (int i = 0; i < m; i++) dfs(heights, i, n-1, atlantic);
+        for (int i = 0; i < n; i++) dfs(heights, m-1, i, atlantic);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] and atlantic[i][j]) {
+                    result.push_back({i,j});
+                }
             }
         }
 
-        return res;
+        return result;
     }
 };
