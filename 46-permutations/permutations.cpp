f8133081602007b675d38f9e5058eb0b57e2c70d
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-02-03 21:38:57 -0500

    Time: 0 ms (100.00%) | Memory: 10.6 MB (50.45%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,22 +1,19 @@
 class Solution {
-    void backtrack(vector<int>& nums, vector<int>& permutation, vector<vector<int>>& res, vector<bool>& is_seen) {
-        if (permutation.size() == nums.size()) res.push_back(permutation);
-        for (int i = 0; i < nums.size(); i++) {
-            if (!is_seen[i]) { // we have not use it
-                is_seen[i] = true;
-                permutation.push_back(nums[i]);
-                backtrack(nums, permutation, res, is_seen);
-                permutation.pop_back();
-                is_seen[i] = false;
-            }
+    void backtrack(vector<int>& nums, int level, vector<vector<int>>& res) {
+        if (level == nums.size()-1) {
+            res.push_back(nums);
+            return;
+        }
+        for (int i = level; i < nums.size(); i++) {
+            swap(nums[i], nums[level]);
+            backtrack(nums, level + 1, res);
+            swap(nums[i], nums[level]);
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<bool> is_seen(nums.size(), false);
         vector<vector<int>> res;
-        vector<int> permutation = {};
-        backtrack(nums, permutation, res, is_seen);
+        backtrack(nums, 0, res);
         return res;
     }
 };
