2efb081c9d689c66e0c8327f4455b0fe68027520
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-02-17 20:34:43 -0500

    Time: 22 ms (81.88%) | Memory: 59.2 MB (91.94%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,30 +1,10 @@
 class Solution {
-    int quick_select(vector<int>& nums, int left, int right, int target) {
-        int res = partition(nums, left, right);
-        if (res == target) return nums[res];
-        else if (res < target) {
-            return quick_select(nums, res+1, right, target);
-        } else {
-            return quick_select(nums, left, res - 1, target);
-        }
-    }
-
-    int partition(vector<int>& nums, int left, int right) {
-        int* pivot = &nums[right];
-        int temp = left;
-        for (int i = left; i < right; i++) {
-            if (nums[i] < *pivot) {
-                swap(nums[temp], nums[i]);
-                temp++;
-            }
-        }
-        swap(*pivot, nums[temp]);
-
-        return temp;
-    }
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        return quick_select(nums, 0, n-1, n-k);
+        if (nums.size() == 1) return nums[0];
+        sort(nums.begin(), nums.end(), [](int n1, int n2){
+            return n1 > n2;
+        });
+        return nums[k-1];
     }
 };
