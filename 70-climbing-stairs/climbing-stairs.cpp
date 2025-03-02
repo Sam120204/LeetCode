f15befcd52a14c61345025e2bbf520fc3beda438
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-03-01 21:55:40 -0500

    Time: 0 ms (100.00%) | Memory: 8.5 MB (34.46%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-if (n <= 2) return n;
-int pre2 = 1, pre1 = 2, cur;
-for (int i = 2; i < n; ++i) {
-cur = pre1 + pre2;
-pre2 = pre1;
-pre1 = cur;
-}
-return cur;
-}
+        vector<int> dp(n+1, 0);
+        //dp[i] means in the current top, the max way to climb
+        // n = 4
+        // [0,1,2,3,5]
+        if (n == 1) return 1;
+        if (n==2) return 2;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < n+1; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        for (auto i : dp) cout << i << " ";
+        return dp[n];
+    }
 };
