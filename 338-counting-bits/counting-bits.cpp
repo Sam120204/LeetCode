123a3a56657bc8d6097c1ed088753d61ab533fb1
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-03-03 23:44:15 -0500

    Time: 0 ms (100.00%) | Memory: 10.6 MB (79.97%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        // 6 - 110
-        // 7 - 111
-        // 8 - 1000
-        // 0 1 1 2 1 2 2 3 1 2 2 2 3 3 4 
-        if (n == 0) return {0};
-        vector<int> dp(n+1, 0);
+        // Key Observations
+        // Every power of two has exactly one 1 in its binary representation.
 
+        // 1 → 0001 → 1 one
+        // 2 → 0010 → 1 one
+        // 4 → 0100 → 1 one
+        // 8 → 1000 → 1 one
+        // Any number that is a power of two starts with a single 1 and the rest are 0s.
+        // Numbers between two consecutive powers of two are formed by adding to previous numbers.
+
+        // The numbers between 4 and 8 (5, 6, 7) are just 1 + (the number of ones in [5-4], [6-4], [7-4]).
+
+        vector<int> dp(n + 1, 0);
+        int powerOfTwo = 1;  // Track the most recent power of two
+        
         for (int i = 1; i <= n; i++) {
-            dp[i] = dp[i >> 1] + (i & 1);
+            if (i == powerOfTwo * 2) { 
+                powerOfTwo = i;  // Update power of two
+            }
+            dp[i] = dp[i - powerOfTwo] + 1;
         }
         
         return dp;
