c3b03fdede2be09cf4f3dc755a072d5b793b721c
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-03-04 19:58:29 -0500

    Time: 0 ms (100.00%) | Memory: 10.9 MB (27.66%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        int n = nums.size();
-        if (n < 3) return 0;
-            vector<int> dp(n, 0);
-        for (int i = 2; i < n; ++i) {
-            if (nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) {
-                dp[i] = dp[i-1] + 1;
+        vector<int>dp(nums.size(),0);
+        for(int i=2;i<nums.size();i++){
+            if (nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
+                dp[i]=dp[i-1]+1;
             }
         }
-        return accumulate(dp.begin(), dp.end(), 0);
+        int ans=0;
+        for (int i=0;i<dp.size();i++){
+            ans+=dp[i];
+        }
+        return ans;
     }
 };
