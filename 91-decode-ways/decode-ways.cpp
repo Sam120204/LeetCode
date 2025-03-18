7b342e728eb3c6c2049ebc52c4e6e481a4b8f725
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-03-18 17:14:30 -0400

    Time: 0 ms (100.00%) | Memory: 8.8 MB (56.22%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int n = s.size();
-        if (n == 0 || s[0] == '0') return 0;
+    if (s.empty() || s[0] == '0') return 0;  // No valid decoding if starts with '0'
 
-        vector<int> dp(n + 1, 0);
-        dp[0] = 1; // Base case: an empty string has one way to be decoded (doing nothing)
-        dp[1] = 1; // Base case: a single character string (not '0') has one way to be decoded
-
-        for (int i = 2; i <= n; ++i) {
-            int oneDigit = stoi(s.substr(i - 1, 1));
-            int twoDigits = stoi(s.substr(i - 2, 2));
-            
-            // If the single digit is valid (i.e., 1-9), add the number of ways to decode the string up to the previous character
-            if (oneDigit >= 1 && oneDigit <= 9) {
-                dp[i] += dp[i - 1];
-            }
-            
-            // If the two-digit number is valid (i.e., 10-26), add the number of ways to decode the string up to the character before the previous character
-            if (twoDigits >= 10 && twoDigits <= 26) {
-                dp[i] += dp[i - 2];
-            }
+    int prev2 = 1, prev1 = 1;  // Equivalent to dp[i-2] and dp[i-1]
+    
+    for (int i = 1; i < s.size(); i++) {
+        int curr = 0;
+        
+        // Single-digit valid decoding (1-9)
+        if (s[i] >= '1' && s[i] <= '9') {
+            curr += prev1;
         }
 
-        return dp[n];
+        // Two-digit valid decoding (10-26)
+        int twoDigit = stoi(s.substr(i-1, 2)); // Convert "s[i-1]s[i]" to integer
+        if (twoDigit >= 10 && twoDigit <= 26) {
+            curr += prev2;
+        }
+
+        prev2 = prev1;
+        prev1 = curr;
     }
+    
+    return prev1;
+}
 };
