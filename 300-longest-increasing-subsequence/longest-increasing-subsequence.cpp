d524e9c55c29b5ff4d88d96c26f467f02514e115
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-03-27 17:45:11 -0400

    Time: 107 ms (38.16%) | Memory: 14.4 MB (45.16%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
-    // int lengthOfLIS(vector<int>& nums) {
-    //     int res = 0;
-    //     if (nums.size() == 1) return 1;
-    //     vector<int> dp(nums.size(), 1);
-    //     for (int i = 0; i < nums.size(); i++) {
-    //         for (int j = 0; j < i; j++) {
-    //             if (nums[i] > nums[j]) {
-    //                 dp[i] = max(dp[i], dp[j]+1);
-    //             }
-    //         }
-    //         res = max(res, dp[i]);
-    //     }
-    //     return res;
-    // }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        if (n<=1) return n;
-        vector<int> dp;
-        dp.push_back(nums[0]);
+        vector<int> dp(n, 1);
+       
         for (int i = 1; i < n; i++) {
-            if (dp.back() < nums[i]) {
-                dp.push_back(nums[i]);
-            } else {
-                auto iter = lower_bound(dp.begin(), dp.end(), nums[i]);
-                *iter = nums[i];
+            for (int j = i; j >= 0; j--) {
+                if (nums[i] > nums[j]) dp[i] = max(dp[i], dp[j]+1);
             }
         }
-        return dp.size();
+        for (auto i : dp) cout << i << " ";
+        return *max_element(dp.begin(), dp.end());
     }
 };
