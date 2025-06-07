3dca3e34a7fdc5889781b5adab6aadc8bf30057a
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-06-06 21:06:08 -0400

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.35%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
@@ -6,28 +6,35 @@ class Solution(object):
         :rtype: List[int]
         """
         nums.sort()
+        # new_nums = sorted(nums) -> copy not in place
 
+        # helper functions:
+        # benefits: 1. code clean + neat; 2. clear logic 3. each function does their own things
+        # first index of the target
         def find_first():
-            left, right = 0, len(nums) - 1
+            left, right = 0, len(nums)-1
             res = -1
+            # perform bianry search
             while left <= right:
                 mid = (left + right) // 2
                 if nums[mid] < target:
                     left = mid + 1
-                else:
+                else: # nums[mid] >= target:
                     if nums[mid] == target:
                         res = mid
-                    right = mid - 1
+                    right = mid -1
             return res
 
+        # find last index of the target
         def find_last():
-            left, right = 0, len(nums) - 1
+            left, right = 0, len(nums)-1
             res = -1
+            # perform bianry search
             while left <= right:
                 mid = (left + right) // 2
                 if nums[mid] > target:
                     right = mid - 1
-                else:
+                else: # nums[mid] <= target:
                     if nums[mid] == target:
                         res = mid
                     left = mid + 1
@@ -36,8 +43,11 @@ class Solution(object):
         first = find_first()
         last = find_last()
 
-        if first == -1:
+        # theres chance that no target exists in the array
+        if (first == -1) or (last == -1):
             return []
 
-        return list(range(first, last + 1))
+        return list(range(first, last+1))
+
+
         
