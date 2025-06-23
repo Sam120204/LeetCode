472926fa2ea0bd61f833ba8937377b10ddc868d4
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-06-22 23:37:12 -0400

    Time: 0 ms (100.00%) | Memory: 22.6 MB (78.33%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        int cur = nums[0], n = nums.size(), count = 1;
-        if (n == 1) return count;
-        auto iter = nums.begin()+1;
-        while (iter != nums.end()) {
-            if (*iter == *(iter-1)) {
-                iter = nums.erase(iter);
-            } else {
-                iter++;
-            }
+    // int removeDuplicates(vector<int>& nums) {
+    //     int cur = nums[0], n = nums.size(), count = 1;
+    //     if (n == 1) return count;
+    //     auto iter = nums.begin()+1;
+    //     while (iter != nums.end()) {
+    //         if (*iter == *(iter-1)) {
+    //             iter = nums.erase(iter);
+    //         } else {
+    //             iter++;
+    //         }
             
+    //     }
+    //     return nums.size();
+    // }
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];  // ✅ Overwrite in-place
+            }
         }
-        return nums.size();
+        return i + 1;  // ✅ Return new length
     }
+
 };
