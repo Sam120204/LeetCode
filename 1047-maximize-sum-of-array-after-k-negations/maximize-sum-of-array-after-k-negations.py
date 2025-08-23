d402be27ddead2f63eef14edb54f8d136866f818
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-08-22 21:33:51 -0400

    Time: 3 ms (69.93%) | Memory: 12.5 MB (33.45%) - LeetSync

--- 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.py
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.py
@@ -5,16 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # 1) Process larger |value| first; smallest |value| ends last
         nums.sort(key=abs, reverse=True)
 
-        # 2) Flip negatives while we have k
         for i in range(len(nums)):
             if nums[i] < 0 and k > 0:
                 nums[i] = -nums[i]
-                k -= 1
-
-        # 3) If k is still odd, flip the smallest |value| once
+                k-=1
+        
         if k % 2 == 1:
             nums[-1] = -nums[-1]
 
