4a86d9bbd0d61daf73ce578c4b16b8a18e0e7a27
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-09-26 20:39:26 -0400

    Time: 275 ms (5.15%) | Memory: 22.9 MB (10.72%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,12 +4,10 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        dp = [float("inf") for i in range(len(prices))] # dp -> min price so far
+        dp = [float("inf") for i in range(len(prices))] # dp[i] we want is min price we have looked so far
         dp[0] = prices[0]
         max_profit = 0
         for i in range(1, len(prices)):
-            dp[i] = min(dp[i-1], prices[i])
+            dp[i] = min(prices[i], dp[i-1])
             max_profit = max(max_profit, prices[i] - dp[i])
         return max_profit
-
-        
