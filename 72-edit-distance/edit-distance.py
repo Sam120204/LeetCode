24840868b7931a5fa91500b1825a72b9e389ee6c
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2025-10-03 21:32:21 -0400

    Time: 98 ms (18.22%) | Memory: 16 MB (10.10%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -5,17 +5,18 @@ class Solution(object):
         :type word2: str
         :rtype: int
         """
-        N,M = len(word1),len(word2)
-        
+        N, M = len(word1), len(word2)
+
         dp = [[0 for j in range(M+1)] for i in range(N+1)]
-        
+
         for i in range(N+1):
             dp[i][0] = i
-        
-        for i in range(M+1):
-            dp[0][i] = i
-            
-        for i in range(1,N+1):
-            for j in range(1,M+1):
-                dp[i][j] = min(dp[i-1][j] + 1,dp[i][j-1] + 1, dp[i-1][j-1] + (1 if word1[i-1] != word2[j-1] else 0))
+
+        for j in range(M+1):
+            dp[0][j] = j
+
+        for i in range(1, N+1):
+            for j in range(1, M+1):
+                dp[i][j] = min(dp[i-1][j] + 1, dp[i][j-1] + 1, dp[i-1][j-1] + (1 if word1[i-1] != word2[j-1] else 0))
+
         return dp[-1][-1]
