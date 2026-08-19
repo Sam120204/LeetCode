88d5218783228f36015123755518ef41f2f22636
Sam Zhong <75916625+Sam120204@users.noreply.github.com>
2026-08-19 11:17:40 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (18.26%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -1,41 +1,34 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isSymmetric(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: bool
-        """
-        if not root:
-            return 0
-
-        q = deque([root])
-
-        while q:
-            current_size = len(q)
-            level = []
-            for i in range(current_size):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                    level.append(node.left.val)
-                else:
-                    level.append(101)
-
-                    
-                if node.right:
-                    q.append(node.right)
-                    level.append(node.right.val)
-                else:
-                    level.append(101)
-                
-            # level = [3, 4, 4, 3]
-            # here to check if it is mirror
-            if level != level[::-1]:
+class Solution:
+    def ifsym(self, arr: List[TreeNode]) -> bool:
+        print("here")
+        for i in range(len(arr)//2):
+            if arr[i].val != arr[len(arr)-i-1].val:
                 return False
-
         return True
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        q = deque([root])
+        while q:
+            size = len(q)
+            if not self.ifsym(q):
+                return False
+            for i in range(size):
+                cur = q.popleft()
+                if cur.val != 101:
+                    if cur.left:
+                        q.append(cur.left)
+                    else:
+                        q.append(TreeNode(101))
+                    if cur.right:
+                        q.append(cur.right)
+                    else:
+                        q.append(TreeNode(101))
+        return True
+
+        
